880e8030919a53f1aba4e6a6a2f4adc98d0d886b
Todd Blankenship <toddmblankenship@gmail.com>
2016-04-04 03:07:59 -0500

    changed User.js to user.js in models

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 var errorHandler = require('errorhandler');
 var lusca = require('lusca');
-//var dotenv = require('dotenv');
+var dotenv = require('dotenv');
 var MongoStore = require('connect-mongo/es5')(session);
 var flash = require('express-flash');
 var path = require('path');
@@ -24,7 +24,7 @@ var upload = multer({ dest: path.join(__dirname, 'uploads') });
  *
  * Default path: .env (You can remove the path argument entirely, after renaming `.env.example` to `.env`)
  */
-//dotenv.load({ path: '.env' });
+dotenv.load();
 
 /**
  * Controllers (route handlers).
