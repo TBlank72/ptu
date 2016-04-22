27c3de0b6bc837b79f89a29c44dba4183144c2df
Todd Blankenship <toddmblankenship@gmail.com>
2016-04-22 17:27:19 -0500

    newrelic moved to config dir, NEW_RELIC_HOME set to 'config/newrelic.js'

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
  * Module dependencies.
  */
-require ('newrelic');
+require ('./config/newrelic');
 var express = require('express');
 var compress = require('compression');
 var session = require('express-session');
